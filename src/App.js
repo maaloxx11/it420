569b32ff5053afec826b5978aaeb7750b8d1b996
maaloxx11 <teerapat.choet@bumail.net>
2020-10-20 11:14:10 +0700

    function1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header.js";
+import Home from "./Home.js";
+import MenuApartment from "./function/Apartment/MenuApartMent.js"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Router>
+					<div className="app">
+						<Switch>
+						
+							<Route exact path="/">
+								<Header></Header>
+								<Home></Home>
+							</Route>
+							<Route exact path="/menuapm">
+								<Header></Header>
+								<MenuApartment></MenuApartment>
+							</Route>
+							<Route exact path="/createroom">
+								<Header></Header>
+								<h1>A</h1>
+							</Route>
+						</Switch>
+					</div>
+				</Router>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+	marginButton: {
+		margin: theme.spacing(2),
+		width: "100%",
+	},
+}));
+
+function Home() {
+	const classes = useStyles();
+	return (
+	
+			<div>
+				<Container maxWidth="md" align="center">
+					<h1>Function</h1>
+					<Link to="/menuapm">
+						<Button
+							variant="contained"
+							color="primary"
+							size="large"
+							className={classes.marginButton}
+						>
+							Function 1
+						</Button>
+					</Link>
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 2
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 3
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 4
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 5
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 6
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 7
+					</Button>
+
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 8
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						size="large"
+						className={classes.marginButton}
+					>
+						Function 9
+					</Button>
+					
+				</Container>
+			</div>
+	);
+}
+
+export default Home;
